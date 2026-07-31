39bf9cafbb36e9a01953a8c11b2e503c0b932c03
Loek Gerrits <99460608+LDGerrits@users.noreply.github.com>
2026-07-31 03:24:27 +0200

    fix packages

--- docs/source.config.ts
+++ docs/source.config.ts
@@ -1,16 +1,11 @@
 import { defineConfig, defineDocs } from "fumadocs-mdx/config";
-import { metaSchema, pageSchema } from "fumadocs-core/source/schema";
 
 export const docs = defineDocs({
 	dir: "content/docs",
 	docs: {
-		schema: pageSchema,
 		postprocess: {
 			includeProcessedMarkdown: true,
 		},
-	},
-	meta: {
-		schema: metaSchema,
 	},
 });
 
