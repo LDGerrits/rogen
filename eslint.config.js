30bd776af0b4ee811d85f263b05644cfc0b1d0a1
Loek Gerrits <99460608+LDGerrits@users.noreply.github.com>
2026-08-16 17:24:25 +0200

    remove examples

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,7 @@ export default defineConfig(
 	eslint.configs.recommended,
 	...tseslint.configs.recommended,
 	{
-		ignores: ["dist/", "examples/", "node_modules/"],
+		ignores: ["dist/", "node_modules/"],
 	},
 	{
 		files: ["src/**/*.ts", "src/**/*.js"],
